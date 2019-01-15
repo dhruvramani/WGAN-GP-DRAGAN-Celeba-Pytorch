4f422fd65409621eb5b6ce5b312cac34c37f3669
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-01-15 14:13:45 +0530

    cuda error

--- train_celeba_wgan_gp.py
+++ train_celeba_wgan_gp.py
@@ -13,6 +13,7 @@ import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 import utils
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def gradient_penalty(x, y, f):
     # interpolation
@@ -104,6 +105,7 @@ for epoch in range(start_epoch, epochs):
         imgs = Variable(imgs)
         bs = imgs.size(0)
         z = Variable(torch.randn(bs, z_dim))
+        print(imgs, z)
         imgs, z = utils.cuda([imgs, z])
 
         f_imgs = G(z)
